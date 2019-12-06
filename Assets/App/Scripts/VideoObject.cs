c6d0d0dca4419c4f2c1a66dc696d93cadb029c5b
tejasVR <tejasshroff24@gmail.com>
2019-12-05 22:52:20 -0500

    connect video object in inspector

---
+++ Assets/App/Scripts/VideoObject.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace XPO
+{
+    [RequireComponent(typeof(ARObject))]
+    public class VideoObject : MonoBehaviour
+    {
+        public GameObject videoTexture;
+        public VideoPlayer videoPlayer;
+
+        private ARObject arObject;
+
+        private void Awake()
+        {
+            arObject = GetComponent<ARObject>();
+        }
+
+        private void OnEnable()
+        {
+            arObject.StartAugmentationCallback += StartAugmment;
+            arObject.StopAugmentationCallback += StopAugment;
+        }
+
+        private void OnDisable()
+        {
+            arObject.StartAugmentationCallback -= StartAugmment;
+            arObject.StopAugmentationCallback -= StopAugment;
+        }
+
+        void Start()
+        {
+            videoTexture.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (videoPlayer == null)
+            {
+                videoPlayer = GetComponentInChildren<VideoPlayer>();
+                videoPlayer.Pause();
+            }
+        }
+
+        private void StartAugmment()
+        {
+            PlayVideo();
+        }
+
+        private void StopAugment()
+        {
+            StopVideo();
+        }
+
+        private void PlayVideo()
+        {
+            if (!videoPlayer.isPlaying)
+            {
+                videoTexture.SetActive(true);
+                videoPlayer.Play();
+            }
+
+        }
+
+        private void StopVideo()
+        {
+            if (videoPlayer != null)
+            {
+                if (videoPlayer.isPlaying)
+                {
+                    videoTexture.SetActive(false);
+                    videoPlayer.Pause();
+                    //video.time = 0;
+                }
+            }
+        }
+    }
+}
+
+
