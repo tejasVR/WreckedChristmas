c3d1e4ebf5ce4978fdd4de5b5a6bcbacf87d248f
tejasVR <tejasshroff24@gmail.com>
2019-12-05 22:11:40 -0500

    adding video and reorganizing assets

---
+++ Assets/App/Scripts/AnimationSpeed.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationSpeed : MonoBehaviour
+{
+    public float animationSpeed;
+    
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        anim.speed = animationSpeed;
+    }
+}
