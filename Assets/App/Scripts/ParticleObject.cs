a186316116914763af26286ddd68851235ab7bf0
tejasVR <tejasshroff24@gmail.com>
2019-12-05 23:35:22 -0500

    adding rick-snow

---
+++ Assets/App/Scripts/ParticleObject.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using XPO;
+
+[RequireComponent(typeof(ARObject))]
+public class ParticleObject : MonoBehaviour
+{    
+    public ParticleSystem particleSystem;
+
+    private ARObject arObject;
+
+    private void Awake()
+    {
+        arObject = GetComponent<ARObject>();
+    }
+
+    private void OnEnable()
+    {
+        arObject.TrackingFoundCallback += StartAugmment;
+        arObject.TrackingLostCallback += StopAugment;
+    }
+
+    private void OnDisable()
+    {
+        arObject.TrackingFoundCallback -= StartAugmment;
+        arObject.TrackingLostCallback -= StopAugment;
+    }
+
+    void Start()
+    {
+        particleSystem.Stop();
+    }
+  
+    private void StartAugmment()
+    {
+        PlayParticle();
+    }
+
+    private void StopAugment()
+    {
+        StopParticle();
+    }
+
+    private void PlayParticle()
+    {
+        if (!particleSystem.isPlaying)
+        {
+            particleSystem.Play();
+        }
+
+    }
+
+    private void StopParticle()
+    {
+        if (particleSystem != null)
+        {
+            if (particleSystem.isPlaying)
+            {
+                particleSystem.Pause();
+            }
+        }
+    }
+}
+
